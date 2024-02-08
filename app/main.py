1bff2948c99a2809fdcf218d7e5cc07520722270
neeearina <arishschkarussia@gmail.com>
2024-02-08 09:39:46 +0500

    fix data encoding

--- app/main.py
+++ app/main.py
@@ -10,3 +10,10 @@ app.include_router(routers.clubs.router)
 @app.get("/")
 def root():
     return {"message": "Root of eduAPI"}
+
+
+@app.middleware("http")
+async def add_process_time_header(request: fastapi.Request, call_next):
+    response = await call_next(request)
+    response.headers["Content-Type"] = "text/html; charset=utf-8"
+    return response
