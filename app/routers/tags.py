9555286ce667290b5550fd78b09d9c6f316a9f5a
neeearina <arishschkarussia@gmail.com>
2024-02-16 19:23:08 +0500

    put/delete for tag routers

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -33,3 +33,51 @@ def create_tag(tag_info: shemas.TagBase):
             my_session.query(session.Tags)
             .filter_by(id=new_tag.id.first())
         )
+
+
+@router.get("/{tag_id}", response_model=shemas.Tag)
+def get_tag_by_id(tag_id: int):
+    """Получить тег по id"""
+    with session.Session() as my_session:
+        tag = (
+            my_session.query(session.Tags)
+            .filter_by(id=tag_id).first()
+        )
+        if not tag:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=f"no tag with id: {tag_id}",
+            )
+        return tag
+
+
+@router.put("/{tag_id}", response_model=shemas.Tag)
+def put_tag_by_id(tag_id: int, update_tag_info: shemas.TagBase):
+    """Изменить тег по его id"""
+    with session.Session() as my_session:
+        tag_query = my_session.query(session.Tags).filter_by(id=tag_id)
+        tag = tag_query.first()
+        if not tag:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=f"tag with id: {tag_id} was not found",
+            )
+        tag_query.update(update_tag_info.dict(), synchronize_session=False)
+        my_session.commit()
+        return tag_query.first()
+
+
+@router.delete("/{tag_id}", status_code=fastapi.status.HTTP_204_NO_CONTENT)
+def delete_tag_by_id(tag_id: int):
+    """Удалить тег по его id"""
+    with session.Session() as my_session:
+        tag_query = my_session.query(session.Tags).filter_by(id=tag_id)
+        tag = tag_query.first()
+        if not tag:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=f"tag with id: {tag_id} was not found",
+            )
+        tag_query.delete(synchronize_session=False)
+        my_session.commit()
+        return fastapi.Response(status_code=fastapi.status.HTTP_204_NO_CONTENT)
