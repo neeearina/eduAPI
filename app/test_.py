fce14914cb1743d094448763c7595b9156edf253
neeearina <arishschkarussia@gmail.com>
2024-02-22 09:57:34 +0500

    only one test(

---
+++ app/test_.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+import main
+
+client = TestClient(main.app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Root of eduAPI"}
